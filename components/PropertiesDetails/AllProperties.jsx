6009933a93cf69f8763bbb20484d89f9902cac09
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-11-22 12:49:45 +0000

    switching to the contact page

--- components/PropertiesDetails/AllProperties.jsx
+++ components/PropertiesDetails/AllProperties.jsx
@@ -1,20 +1,89 @@
+import Image from "next/image";
+import { PiBathtubLight } from "react-icons/pi";
+import { CiLocationOn } from "react-icons/ci";
+import { IoBedOutline } from "react-icons/io5";
+import { BsXSquare } from "react-icons/bs";
 
-import Image from 'next/image'
-const AllProperties = () => {
+const AllProperties = ({ status, featured }) => {
+  const checkstatus = (status) => {
+    let color = "";
+    switch (status) {
+      case "FOR SALE":
+        color = "bg-red-500";
+        return color;
+      case "FOR RENT":
+        color = "bg-green-400";
+        return color;
+    }
+  };
+
+  const checkFeature = (featured) => {
+    var color;
+    if (featured === "FEATURED") {
+      color = "bg-white";
+      return color;
+    } else {
+      color = "";
+      return color;
+    }
+  };
   return (
-    <div className='relative overflow-hidden rounded-lg'>
-         <Image className=' relative hover:scale-105 hover:transition hover:duration-300 hover:ease-in-out ' src="/gallery/gallery-3.jpg" width={1500} height={1000} alt="image" />
-         <p className='absolute top-4 right-4 bg-green-400 text-white p-1 rounded-lg px-2 text-sm'>FOR SALE </p>
-         <div className='absolute flex justify-between items-center w-[100%] bottom-0 py-3 p-1 rounded-lg px-2 text-sm backdrop-blur-sm bg-white/30 '>
-            <div><p className='text-white'>$4,000,000</p></div>
-            <div><p className='text-white'>8 Beds</p></div>
-            <div><p className='text-white'>2 Baths</p></div>
-            <div><p className='text-white'>1 sqft</p></div>
-         </div>
-         <div className='absolute bottom-20 '> <div><p className='text-white'>Northridge Community</p></div></div>
-         <p className='absolute bottom-32 text-white'>3-Bedroom Townhouse</p>
+    <div className="relative overflow-hidden rounded-lg">
+      <Image
+        className=" relative hover:scale-105 hover:transition hover:duration-300 hover:ease-in-out "
+        src="/gallery/gallery-3.jpg"
+        width={1500}
+        height={1000}
+        alt="image"
+      />
+      <div>
+        <p
+          className={`absolute top-4 left-4 ${checkFeature(
+            featured
+          )} p-1 rounded-lg px-2 text-sm`}
+        >
+          {featured}
+        </p>
+        <p
+          className={`absolute top-4 right-4 ${checkstatus(
+            status
+          )} text-white p-1 rounded-lg px-2 text-sm`}
+        >
+          {status}
+        </p>
+      </div>
+      <div className="absolute flex justify-between items-center w-[100%] bottom-0 py-3 p-1 rounded-lg px-2 text-sm backdrop-blur-sm bg-white/30 ">
+        <div>
+          <p className="text-white text-2xl">$4,000,000</p>
+        </div>
+        <div
+          className="flex justify-evenly
+         gap-5"
+        >
+          <div className="flex justify-center gap-1 items-center">
+            <IoBedOutline size={30} className="text-primarylight" />
+            <p className="text-white">8 Beds</p>
+          </div>
+          <div className="flex justify-center gap-1 items-center">
+            <PiBathtubLight size={30} className="text-primarylight" />
+            <p className="text-white">2 Bath</p>
+          </div>
+          <div className="flex justify-center gap-1 items-center">
+            <BsXSquare size={25} className="text-primarylight" />
+            <p className="text-white">1 sqft</p>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-20 ">
+        {" "}
+        <div className="flex pl-5 gap-2 items-center justify-center">
+          <CiLocationOn size={25} className="text-white" />
+          <p className="text-white ">Northridge Community</p>
+        </div>
+      </div>
+      <p className="absolute bottom-32 text-white pl-5">3-Bedroom Townhouse</p>
     </div>
-  )
-}
+  );
+};
 
-export default AllProperties
+export default AllProperties;
--- pages/properties/[category]/[propertyId].js
+++ pages/properties/[category]/[propertyId].js
@@ -4,11 +4,11 @@ import AllProperties from "../../../components/PropertiesDetails/AllProperties";
 // import SwiperComponent from '../../../components/PropertiesDetails/SwiperComponent'
 import { LiaBedSolid } from "react-icons/lia";
 import {
-	PiBathtubLight,
-	PiCarDuotone,
-	PiTree,
-	PiSwimmingPoolThin,
-	PiDoorOpen,
+  PiBathtubLight,
+  PiCarDuotone,
+  PiTree,
+  PiSwimmingPoolThin,
+  PiDoorOpen,
 } from "react-icons/pi";
 import { BsPlusSquare } from "react-icons/bs";
 import { GiCctvCamera } from "react-icons/gi";
@@ -22,192 +22,202 @@ import { FiWifi } from "react-icons/fi";
 import Header from "../../../components/shared/Header";
 
 function PropertyDetail() {
-	const router = useRouter()
-  const {propertyId} = router.query;
-	const id = parseInt(propertyId);
+  const router = useRouter();
+  const { propertyId } = router.query;
+  const id = parseInt(propertyId);
 
-	const property = properties.find(property => property.id === id);
-  const description = "Modern and bright, this downtown apartment offers proximity to the finest restaurants and entertainment";
-	return (
-		<>
-		<Header summaryText={property.price} title={property.description} description={description} image={`listing-${id}`} cutout >
-			<button className="btn-primary rounded-lg mt-10">Request info</button>
-		</Header>
-		<div className="general-margin mt-20">
-			<div className="grid grid-cols-2 gap-5">
-				<Gallery id={"1"} />
-				<Gallery id={"2"} />
-				<Gallery id={"3"} />
-				<Gallery id={"4"} />
-			</div>
+  const property = properties.find((property) => property.id === id);
+  const description =
+    "Modern and bright, this downtown apartment offers proximity to the finest restaurants and entertainment";
+  return (
+    <>
+      <Header
+        summaryText={property.price}
+        title={property.description}
+        description={description}
+        image={`listing-${id}`}
+        cutout
+      >
+        <button className="btn-primary rounded-lg mt-10">Request info</button>
+      </Header>
+      <div className="general-margin mt-20">
+        <div className="grid grid-cols-2 gap-5">
+          <Gallery id={"1"} />
+          <Gallery id={"2"} />
+          <Gallery id={"3"} />
+          <Gallery id={"4"} />
+        </div>
 
-			{/* properties details */}
-			<div className="flex lg:flex-row md:flex-col sm:flex-col justify-start items-center gap-10">
-				<div className="lg:w-[60%] md:w-[100%] sm:w-[100%]">
-					<div className="">
-						<p className="border-2 border-primary rounded-2xl p-1 text-primary w-fit">
-							<span className="w-50 h-50 rounded-lg bg-primary" />
-							Properties Details
-						</p>
-					</div>
+        {/* properties details */}
+        <div className="flex lg:flex-row md:flex-col sm:flex-col justify-start items-center gap-10">
+          <div className="lg:w-[60%] md:w-[100%] sm:w-[100%]">
+            <div className="">
+              <p className="border-2 border-primary rounded-2xl p-1 text-primary w-fit">
+                <span className="w-50 h-50 rounded-lg bg-primary" />
+                Properties Details
+              </p>
+            </div>
 
-					<div className="text-red-500 flex justify-between items-center lg:w-[80%] md:w-[80%] sm:w-full my-8">
-						<div className="text-primary flex justify-center items-center gap-3">
-							<LiaBedSolid size={30} />
-							<p className="text-primary">{property.beds} Bed</p>
-						</div>
-						<div className="text-primary flex justify-center items-center gap-3">
-							<PiBathtubLight size={30} />
-							<p className="text-primary">{property.baths} Baths</p>
-						</div>
-						<div className="text-primary flex justify-center items-center gap-3">
-							<BsPlusSquare size={30} />{" "}
-							<p className="text-primary">{property.size} sqft</p>
-						</div>
-						<div className="text-primary flex justify-center items-center gap-3">
-							<PiCarDuotone size={30} />{" "}
-							<p className="text-primary">{property.parkingspots} Parking spots</p>
-						</div>
-					</div>
-					<hr className="my-10" />
+            <div className="text-red-500 flex justify-between items-center lg:w-[80%] md:w-[80%] sm:w-full my-8">
+              <div className="text-primary flex justify-center items-center gap-3">
+                <LiaBedSolid size={30} />
+                <p className="text-primary">{property.beds} Bed</p>
+              </div>
+              <div className="text-primary flex justify-center items-center gap-3">
+                <PiBathtubLight size={30} />
+                <p className="text-primary">{property.baths} Baths</p>
+              </div>
+              <div className="text-primary flex justify-center items-center gap-3">
+                <BsPlusSquare size={30} />{" "}
+                <p className="text-primary">{property.size} sqft</p>
+              </div>
+              <div className="text-primary flex justify-center items-center gap-3">
+                <PiCarDuotone size={30} />{" "}
+                <p className="text-primary">
+                  {property.parkingspots} Parking spots
+                </p>
+              </div>
+            </div>
+            <hr className="my-10" />
 
-					<div>
-						<div>
-							<h3 className="mb-8">Regarding the Property:</h3>
-							<p>
-								In a symphony of elegance and tranquility, this property reveals
-								itself, nestling amidst the serene contours of its surroundings.
-								The facade, a harmonious blend of sophistication and simplicity,
-								whispers tales of design excellence. Each nook and cranny
-								resonate with the melody of high-quality craftsmanship, the
-								walls telling tales of unparalleled comfort and timeless style.
-							</p>
-						</div>
+            <div>
+              <div>
+                <h3 className="mb-8">Regarding the Property:</h3>
+                <p>
+                  In a symphony of elegance and tranquility, this property
+                  reveals itself, nestling amidst the serene contours of its
+                  surroundings. The facade, a harmonious blend of sophistication
+                  and simplicity, whispers tales of design excellence. Each nook
+                  and cranny resonate with the melody of high-quality
+                  craftsmanship, the walls telling tales of unparalleled comfort
+                  and timeless style.
+                </p>
+              </div>
 
-						<div>
-							<ul className="list-decimal my-12">
-								<li>Sed consequat leo</li>
-								<li>Vestibulum purus quam scelerisque</li>
-								<li>Vestibulum turpis sem aliquet eget</li>
-								<li>Ut non enim</li>
-								<li>Vivamus quis mi</li>
-							</ul>
+              <div>
+                <ul className="list-decimal my-12">
+                  <li>Sed consequat leo</li>
+                  <li>Vestibulum purus quam scelerisque</li>
+                  <li>Vestibulum turpis sem aliquet eget</li>
+                  <li>Ut non enim</li>
+                  <li>Vivamus quis mi</li>
+                </ul>
 
-							<p>
-								A gateway to luxury, the property unfolds a canvas where every
-								stroke is a brush of meticulous detail, painting a world where
-								elegance meets functionality. It’s not just a dwelling, but a
-								realm where every square inch is a testament to excellence in
-								architecture, a space where the symphony of luxury and comfort
-								plays on a continuous loop, offering an unrivaled living
-								experience.
-							</p>
-						</div>
-						<hr className="my-12" />
+                <p>
+                  A gateway to luxury, the property unfolds a canvas where every
+                  stroke is a brush of meticulous detail, painting a world where
+                  elegance meets functionality. It’s not just a dwelling, but a
+                  realm where every square inch is a testament to excellence in
+                  architecture, a space where the symphony of luxury and comfort
+                  plays on a continuous loop, offering an unrivaled living
+                  experience.
+                </p>
+              </div>
+              <hr className="my-12" />
 
-						<div className="grid grid-cols-3 gap-y-[10%]">
-							<div className="text-primary flex justify-start items-center gap-3">
-								<PiTree size={30} />
-								<p className="text-primary">Garden</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<GiCctvCamera size={30} />
-								<p className="text-primary">Security Camera</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<MdOutlineLocalLaundryService size={30} />
-								<p className="text-primary">Laundry</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<FiWifi size={30} /> <p className="text-primary">Internet</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<PiSwimmingPoolThin size={30} />
-								<p className="text-primary">Pool</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<RiParkingBoxLine size={30} />
-								<p className="text-primary">Garage</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<PiBathtubLight size={30} />
-								<p className="text-primary">Jacuzzi</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<PiDoorOpen size={30} />
-								<p className="text-primary">Elevator</p>
-							</div>
-							<div className="text-primary flex justify-start items-center gap-3">
-								<CiForkAndKnife size={30} />
-								<p className="text-primary">Dish Washer</p>
-							</div>
-						</div>
-					</div>
-				</div>
+              <div className="grid grid-cols-3 gap-y-[10%]">
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <PiTree size={30} />
+                  <p className="text-primary">Garden</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <GiCctvCamera size={30} />
+                  <p className="text-primary">Security Camera</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <MdOutlineLocalLaundryService size={30} />
+                  <p className="text-primary">Laundry</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <FiWifi size={30} /> <p className="text-primary">Internet</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <PiSwimmingPoolThin size={30} />
+                  <p className="text-primary">Pool</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <RiParkingBoxLine size={30} />
+                  <p className="text-primary">Garage</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <PiBathtubLight size={30} />
+                  <p className="text-primary">Jacuzzi</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <PiDoorOpen size={30} />
+                  <p className="text-primary">Elevator</p>
+                </div>
+                <div className="text-primary flex justify-start items-center gap-3">
+                  <CiForkAndKnife size={30} />
+                  <p className="text-primary">Dish Washer</p>
+                </div>
+              </div>
+            </div>
+          </div>
 
-				<div className="lg:w-[30%] md:w-full sm:w-full border p-5">
-					<div className="p-x-5">
-						<div>
-							<p className="bg-red-500 text-white p-2 rounded-lg w-fit my-4">
-								{property.type}
-							</p>
-							<h1>{property.price}</h1>
-						</div>
-						<hr className="mt-4" />
-						<div>
-							<h4 className=" my-4">Get in touch to receive more info</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</div>
-						<div>
-							<form className="w-full mt-4">
-								<input
-									className="border p-2 py-4 rounded w-full"
-									type="text"
-									placeholder="John Doe"
-								/>{" "}
-								<br />
-								<input
-									className="border p-2 py-4 rounded my-5 w-full"
-									type="text"
-									placeholder="contact@gmail.com"
-								/>{" "}
-								<br />
-								<input
-									className="border p-2 rounded w-full py-4"
-									type="text"
-									placeholder="+1 345-678"
-								/>{" "}
-								<br /> <br />
-								<button className="justify-center border w-full text-center py-4 rounded hover:bg-primary hover:text-white animate">
-									Get in Touch
-								</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
+          <div className="lg:w-[30%] md:w-full sm:w-full border p-5">
+            <div className="p-x-5">
+              <div>
+                <p className="bg-red-500 text-white p-2 rounded-lg w-fit my-4">
+                  {property.type}
+                </p>
+                <h1>{property.price}</h1>
+              </div>
+              <hr className="mt-4" />
+              <div>
+                <h4 className=" my-4">Get in touch to receive more info</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+              <div>
+                <form className="w-full mt-4">
+                  <input
+                    className="border p-2 py-4 rounded w-full"
+                    type="text"
+                    placeholder="John Doe"
+                  />{" "}
+                  <br />
+                  <input
+                    className="border p-2 py-4 rounded my-5 w-full"
+                    type="text"
+                    placeholder="contact@gmail.com"
+                  />{" "}
+                  <br />
+                  <input
+                    className="border p-2 rounded w-full py-4"
+                    type="text"
+                    placeholder="+1 345-678"
+                  />{" "}
+                  <br /> <br />
+                  <button className="justify-center border w-full text-center py-4 rounded hover:bg-primary hover:text-white animate">
+                    Get in Touch
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
 
-			{/* Browse all properties */}
-			<div>
-				<div className="">
-					<p className="border-2 border-primary rounded-2xl p-1 text-primary w-fit">
-						<span className="w-50 h-50 rounded-lg bg-primary" />
-						Related Property
-					</p>
-				</div>
-				<h1>Browse all properties</h1>
+        {/* Browse all properties */}
+        <div>
+          <div className="">
+            <p className="border-2 border-primary rounded-2xl p-1 text-primary w-fit">
+              <span className="w-50 h-50 rounded-lg bg-primary" />
+              Related Property
+            </p>
+          </div>
+          <h1>Browse all properties</h1>
 
-				<div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-5">
-					<AllProperties />
-					<AllProperties />
-					<AllProperties />
-					<AllProperties />
-				</div>
-			</div>
-			{/* <SwiperComponent /> */}
-		</div>
-		</>
-	);
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 sm-max:grid-cols-1 gap-5">
+            <AllProperties status="FOR SALE" />
+            <AllProperties status="FOR SALE" />
+            <AllProperties status="FOR RENT" />
+            <AllProperties status="FOR RENT" featured="FEATURED" />
+          </div>
+        </div>
+        {/* <SwiperComponent /> */}
+      </div>
+    </>
+  );
 }
 
 export default PropertyDetail;
