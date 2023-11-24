49fb67be59c4ee8ef086cedba11f7fc12d995a62
dm-mensavi <mensavidavid@gmail.com>
2023-11-24 22:24:19 +0000

    fix uneven testimonials height

--- components/home/ReviewCard.js
+++ components/home/ReviewCard.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function ReviewCard(props) {
   const stars = Array(props.review.starRating).fill(null);
   return (
-    <div className='card bg-white border rounded-lg shadow-customC'>
+    <div className='card bg-white min-h-[300px] border rounded-lg shadow-customC'>
         <div className='card-content py-[35px] px-[40px] flex flex-col justify-center items-start'>
             <div className='star-ratings flex gap-2'>
                 {stars.map((star, index) => (
