493ff3b1e1625393d219a394833384588494be09
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-11-29 00:19:12 +0000

    Done with the size

--- components/PropertiesDetails/Modal.jsx
+++ components/PropertiesDetails/Modal.jsx
@@ -1,14 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const Modal = ({ closeModal, imageSrc }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70" onClick={closeModal}>
-      <div className="max-w-[60vw] max-h-[70vh] mx-auto p-4 rounded-lg overflow-hidden">
-        <Image src={imageSrc} alt="modal" width={1600} height={1000} className="w-auto h-auto"  />
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70"
+      onClick={closeModal}
+    >
+      <div className="lg:w-[60vw] lg:h-[70vh] md:w-[70vw] md:h-[80vh] sm:w-[90vw] sm:h-[90vh] mx-auto p-4 rounded-lg overflow-hidden">
+        <Image
+          src={imageSrc}
+          alt="modal"
+          width={1600}
+          height={1000}
+          className="w-auto h-auto"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
-
+export default Modal;
